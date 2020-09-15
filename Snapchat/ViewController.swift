98130117620d583a9bd083d1ff896cc4d24427a5
Daiane Babicz <53682876+daianebabicz@users.noreply.github.com>
2020-09-15 03:36:21 -0300

    Add files via upload

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Jamilton  Damasceno
+//  Copyright © Curso Apple Watch. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let autenticacao = Auth.auth()
+        
+        /*do {
+            try autenticacao.signOut()
+        } catch {
+            print("Erro ao deslogar usuario")
+        }*/
+        
+        autenticacao.addStateDidChangeListener { (autenticacao, usuario) in
+            
+            if let usuarioLogado = usuario {
+                self.performSegue(withIdentifier: "loginAutomaticoSegue", sender: nil)
+            }
+            
+        }
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
